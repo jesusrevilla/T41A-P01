ceb05136600de863067617b06e5f9f6fb1a31c54
daf01-web <171513@upslp.edu.mx>
2025-08-27 19:14:34 -0600

    Update setup.sql

--- setup.sql
+++ setup.sql
@@ -1,8 +1,8 @@
 CREATE TABLE estudiante (
     id SERIAL PRIMARY KEY,
     nombre VARCHAR(20),
-    matricula VARCHAR(4),
-    carrera VARCHAR(20)
+    matricula VARCHAR(4) UNIQUE,
+    carrera VARCHAR(20) 
 );
 
 CREATE TABLE libro (
@@ -10,7 +10,7 @@ CREATE TABLE libro (
     titulo VARCHAR(100),
     autor VARCHAR(100),
     anio_publicacion INT,
-    isbn VARCHAR(20)
+    isbn VARCHAR(20) UNIQUE
 );
 
 CREATE TABLE prestamo (
@@ -20,4 +20,28 @@ CREATE TABLE prestamo (
     fecha_prestamo DATE,
     fecha_devolucion DATE,
     estado VARCHAR(20) CHECK (estado IN ('activo', 'devuelto'))
+);
+
+SELECT libro.titulo, estudiante.nombre, prestamo.fecha_prestamo
+FROM prestamo
+JOIN libro ON prestamo.libro_id = libro.id
+JOIN estudiante ON prestamo.estudiante_id = estudiante.id
+WHERE prestamo.estado = 'activo';
+
+
+SELECT DISTINCT estudiante.nombre
+FROM prestamo
+JOIN estudiante ON prestamo.estudiante_id = estudiante.id
+WHERE prestamo.estado = 'devuelto';
+
+
+SELECT DISTINCT estudiante.nombre
+FROM prestamo
+JOIN estudiante ON prestamo.estudiante_id = estudiante.id
+WHERE prestamo.estado = 'devuelto';
+
+SELECT titulo
+FROM libro
+WHERE id NOT IN (
+    SELECT libro_id FROM prestamo
 );
