fb9e3fd66e7a26c267e417838fb22adac970d2bf
daf01-web <171513@upslp.edu.mx>
2025-08-27 19:10:54 -0600

    Update setup.sql

--- setup.sql
+++ setup.sql
@@ -1,1 +1,23 @@
+CREATE TABLE estudiante (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(20),
+    matricula VARCHAR(4),
+    carrera VARCHAR(20)
+);
 
+CREATE TABLE libro (
+    id SERIAL PRIMARY KEY,
+    titulo VARCHAR(100),
+    autor VARCHAR(100),
+    anio_publicacion INT,
+    isbn VARCHAR(20)
+);
+
+CREATE TABLE prestamo (
+    id SERIAL PRIMARY KEY,
+    estudiante_id INT REFERENCES estudiante(id),
+    libro_id INT REFERENCES libro(id),
+    fecha_prestamo DATE,
+    fecha_devolucion DATE,
+    estado VARCHAR(20) CHECK (estado IN ('activo', 'devuelto'))
+);
