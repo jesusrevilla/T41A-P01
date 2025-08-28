8476a0a352eed61b159b5d6d0b71fe2bfc2f6343
aldogg10 <179862@upslp.edu.mx>
2025-08-27 19:15:33 -0600

    Update setup.sql

--- setup.sql
+++ setup.sql
@@ -1,1 +1,25 @@
+CREATE TABLE libro(
+  id SERIAL PRIMARY KEY,
+  titulo text not null,
+  autor text not null,
+  anio_publicacion INTEGER not null,
+  isbn text not null
 
+);
+
+CREATE TABLE estudiante(
+  id SERIAL PRIMARY KEY,
+  nombre text not null,
+  matricula text not null,
+  carrera text not null
+);
+
+CREATE TABLE prestamo(
+  id_prestamo SERIAL PRIMARY KEY,
+  estudiante_id INTEGER REFERENCES estudiante(id) not null,
+  libro_id INTEGER REFERENCES libro(id) not null,
+  fecha_prestamo DATE NOT NULL,
+  fecha_devolucion DATE,
+  estado varchar(16) 
+
+);
