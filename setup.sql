1be591a66a883811dd21b245c80857feeb148bc2
YaelQCI <181914@upslp.edu.mx>
2025-08-27 19:06:38 -0600

    feature/181914

--- setup.sql
+++ setup.sql
@@ -1,1 +1,23 @@
+CREATE TABLE estudiante(
+  id SERIAL PRIMARY KEY,
+  nombre VARCHAR(30) NOT NULL,
+  matricula VARCHAR(4) NOT NULL,
+  carrera VARCHAR(20) NOT NULL
+);
 
+CREATE TABLE libro(
+  id SERIAL PRIMARY KEY,
+  titulo VARCHAR(30) NOT NULL,
+  autor VARCHAR(50) NOT NULL,
+  anio_publicacion INTEGER NOT NULL,
+  isbn VARCHAR(14) NOT NULL
+);
+
+CREATE TABLE prestamo(
+  prestamo_id SERIAL PRIMARY KEY,
+  estudiante_id INTEGER REFERENCES estudiante(id) NOT NULL,
+  libro_id INTEGER REFERENCES libro(id) NOT NULL,
+  fecha_prestamo DATE NOT NULL,
+  fecha_devolucion DATE,
+  estado VARCHAR(8)
+);
