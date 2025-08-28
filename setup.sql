888507d5891a7e31b3c33aa2563972d04bb97edf
Bryan Yareth Gámez Contreras <96131248+BryanYarethGC@users.noreply.github.com>
2025-08-27 19:09:55 -0600

    feature/182934

--- setup.sql
+++ setup.sql
@@ -1,1 +1,48 @@
+CREATE TABLE estudiante(
+ id SERIAL PRIMARY KEY,
+ nombre VARCHAR(50) NOT NULL,
+ matricula VARCHAR(4) NOT NULL,
+ carrera VARCHAR(50)
+);
 
+CREATE TABLE libro(
+  id SERIAL PRIMARY KEY,
+  titulo VARCHAR(100) NOT NULL,
+  autor VARCHAR(50) NOT NULL,
+  anio_publicacion INTEGER NOT NULL,
+  isbn VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE prestamo(
+  id_prestamo SERIAL PRIMARY KEY,
+  estudiante_id INTEGER REFERENCES estudiante(id),
+  libro_id INTEGER REFERENCES libro(id),
+  fecha_prestamo DATE NOT NULL,
+  fecha_devolucion DATE,
+  estado VARCHAR(15) NOT NULL
+);
+
+-- Insertar estudiantes
+INSERT INTO estudiante (nombre, matricula, carrera) VALUES
+('Ana Torres', 'A001', 'Ingeniería'),
+('Luis Gómez', 'A002', 'Derecho'),
+('María López', 'A003', 'Medicina'),
+('Carlos Ruiz', 'A004', 'Arquitectura'),
+('Sofía Méndez', 'A005', 'Psicología');
+
+-- Insertar libros
+INSERT INTO libro (titulo, autor, anio_publicacion, isbn) VALUES
+('Cien años de soledad', 'Gabriel García Márquez', 1967, '978-0307474728'),
+('El Principito', 'Antoine de Saint-Exupéry', 1943, '978-0156013987'),
+('1984', 'George Orwell', 1949, '978-0451524935'),
+('La Odisea', 'Homero', -800, '978-0140268867'),
+('Rayuela', 'Julio Cortázar', 1963, '978-8437604947');
+
+-- Insertar préstamos
+
+INSERT INTO prestamo (estudiante_id, libro_id, fecha_prestamo, fecha_devolucion, estado) VALUES
+(1, 1, '2025-08-20', NULL, 'activo'),
+(2, 2, '2025-08-15', '2025-08-22', 'devuelto'),
+(3, 3, '2025-08-10', '2025-08-18', 'devuelto'),
+(1, 4, '2025-08-25', NULL, 'activo'),
+(4, 2, '2025-08-26', NULL, 'activo');
