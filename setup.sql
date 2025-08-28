fcf5aa5b8f8286c95c09b146442832e1bb6b7ac9
Bryan Yareth Gámez Contreras <96131248+BryanYarethGC@users.noreply.github.com>
2025-08-27 19:38:00 -0600

    feature/182934

--- setup.sql
+++ setup.sql
@@ -46,3 +46,29 @@ INSERT INTO prestamo (estudiante_id, libro_id, fecha_prestamo, fecha_devolucion,
 (3, 3, '2025-08-10', '2025-08-18', 'devuelto'),
 (1, 4, '2025-08-25', NULL, 'activo'),
 (4, 2, '2025-08-26', NULL, 'activo');
+
+-- 1. Mostrar todos los libros prestados actualmente (estado = 'activo')
+SELECT libro.titulo, estudiante.nombre, prestamo.fecha_prestamo
+FROM prestamo
+JOIN libro ON prestamo.libro_id = libro.id
+JOIN estudiante ON prestamo.estudiante_id = estudiante.id
+WHERE prestamo.estado = 'activo';
+
+-- 2. Listar los estudiantes que han devuelto al menos un libro
+SELECT DISTINCT estudiante.nombre
+FROM prestamo
+JOIN estudiante ON prestamo.estudiante_id = estudiante.id
+WHERE prestamo.estado = 'devuelto';
+
+-- 3. Mostrar cuántos libros ha prestado cada estudiante
+SELECT estudiante.nombre, COUNT(*) AS total_prestamos
+FROM prestamo
+JOIN estudiante ON prestamo.estudiante_id = estudiante.id
+GROUP BY estudiante.nombre;
+
+-- 4. Consultar los libros que nunca han sido prestados
+SELECT titulo
+FROM Libro
+WHERE id NOT IN (
+    SELECT libro_id FROM prestamo
+);
